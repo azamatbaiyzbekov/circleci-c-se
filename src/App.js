ec714d3d52836da4bb3847418f29efa40a8f548c
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2020-01-23 18:26:23 -0800

    adds state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,30 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    console.log('[App.js] constructor')
+  };
+
+  state = {
+    text: [
+      {name: 'CircleCI', founded: 2010}
+    ],
+    showText: false,
+    showButton: true
+  };
+
+
+
+  toggleTextHandler = () => {
+    const doesShow = this.state.showText;
+    this.setState({ showText: !doesShow});
+  };
+  
+  
+  
   render() {
+    console.log('[App.js] render');
     return (
       <div className="App">
        
