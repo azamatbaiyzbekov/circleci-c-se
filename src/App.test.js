f0ccb464acfcf9e2c6d9f2c0f3fc150f5d22310d
Azamat Baiyzbekov <azamatbaiyzbekov@Azamats-MacBook-Pro.local>
2020-01-23 19:25:10 -0800

    adds test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import { act } from "react-dom/test-utils";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Button from './components/Button/Button'
+
+
+let container = null;
+beforeEach(() => {
+  
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  container.remove();
+  container = null;
+});
+
+it("contains string when clicked", () => {
+  const onClick = jest.fn();
+  act(() => {
+    render(<Button onClick={onClick} />, container);
+  });
+
+  // get ahold of the button element, and trigger some clicks on it
+  const element = document.querySelector('Button');
+  expect(element).toHaveTextContent('Text');
+
+  act(() => {
+    element.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+  });
+
+  expect(onClick).toHaveBeenCalledTimes(0);
+  expect(element).toHaveTextContent('Toggle');
+
+  act(() => {
+    for (let i = 0; i < 5; i++) {
+      element.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    }
+  });
+
+  expect(onClick).toHaveBeenCalledTimes(0);
+  expect(element).toHaveTextContent('Toggle Text');
 });
